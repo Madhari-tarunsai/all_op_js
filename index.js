2779f72ba06be1c735ef4e28d5818b01d4c8a5ee
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2024-12-21 14:55:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,134 @@
+// 1.	What is the result of 10 + '5'? 
+// let a=10
+// let b="5"
+// console.log(a +b)
+
+// What is the value of x after x = 10; x += 5;? 
+// let x=10
+// x+=5
+// console.log(x)
+
+// What is the result of 10 - null? 
+// let a=10
+// let b=null
+// console.log(a - b)
+
+
+// What is the value of x after x = 10; x *= 2;? 
+// let x=10
+// x*=2
+// console.log(x)
+
+// 	What is the result of 10 / '2'? 
+// let a=10
+// let b="2"
+// console.log(a/b)
+
+// What is the result of 10 / 3? 
+// let a=10
+// let b=3
+// console.log(a%b)
+
+// 	What is the value of x after x = 10; x = x ** 2;? 
+// let x= 10;
+// x=x**2
+// console.log(x)
+
+// What is the result of 10 > 5 && 5 < 10? 
+// let a=10
+// let b=5
+// console.log((a>b)&&(a<b))
+
+// What is the result of 10 === '10'? 
+// let a=10
+// let b="10"
+// console.log(a==b)
+
+// What is the result of 10 || 'default'? 
+// let a=10
+// let b="defalut"
+// console.log(b||a)
+
+// 	What is the result of !true? 
+// let a=34
+// let b=34
+// console.log(!(a==b))
+
+// 13.	What is the result of 10 & 5? 
+// let a=10
+// let b=5
+// console.log(a&b)
+
+// 14.	What is the result of 10 | 5?
+// let a=10
+// let b=5
+// console.log(a|b) 
+
+// 15.	What is the result of 10 ^ 5? 
+// let a=10
+// let b=5
+// console.log(a^b) 
+
+// What is the result of x = 10; y = ++x;?  /What are the values of x and y? 
+// let x=10;
+// y=++x
+// console.log(x)
+// console.log(y)
+
+// 2.	What is the result of x = 10; console.log(++x);? 
+// let x=10
+// console.log(++x)
+
+
+// 2.	What is the result of x = 10; console.log(x++);? 
+// let x=10
+// console.log(x++)
+
+
+
+// What is the result of x = 10; y = ++x;?  /What are the values of x and y? 
+// let x=10;
+// y=--x
+// console.log(x)
+// console.log(y)
+
+// 2.	What is the result of x = 10; console.log(x--);and what is the value of x? 
+// let x=10
+// console.log(x--)
+// console.log(x)
+
+
+// 1.	What is the result of x = 10; y = x > 5 ? 'yes' : 'no';?  What is the value of y? 
+// let x=10;
+// y=x>5? "yes":"no"
+// console.log(y)
+
+
+// 2.	What is the result of x = 10; y = x > 5 ? 'yes' : x > 3 ? 'maybe' : 'no';?  What is the value of y? 
+// let x=10;
+// y=x>5? "yes":x>3? "maybe":"no"
+// console.log(y)
+ 
+
+// What is the result of null ?? 'default'? 
+// let obj=
+// {
+//     name:"tarunsai",
+//     id:89,
+//     placement:"10000coder",
+    
+// }
+// let company;
+// console.log(obj.company??"xyz")
+
+
+// 1.	What is the result of const obj = { a: { b: 'c' } }; console.log(obj?.a?.b);? 
+// const obj={a:{b:"c"}}
+// console.log(obj?.b?.c)
+
+
+
+
+
+
+
